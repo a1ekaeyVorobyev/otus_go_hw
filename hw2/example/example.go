d9e600ff2fcae11faf1e39da9a2ec9ee34e4bb4f
A1eksey Vorobyev <a1eksey.a1.vorobyev@gmail.com>
2020-02-03 23:44:28 +0300

    first commit

--- hw2/example/example.go
+++ hw2/example/example.go
@@ -8,16 +8,16 @@ import (
 func main() {
 
 	testStrings := map[string]string{
-		//"0123456789": "V",
-		//"a4bc2d5e":   "aaaabccddddde",
-		//"abcd":       "abcd",
-		//"45":         "Не верный формат",
-		//`qwe\4\5`:    "qwe45",
-		`qwe\45`: "qwe44444",
-		//`qwe5\4`:     "qweeeee4",
-		//`qwe\\5`:     `qwe\\\\\`,
-		//`q\we`:       "Не верный формат",
-		//"ab/c":       "ab/c",
+		"0123456789": "Не верный формат",
+		"a4bc2d5e":   "aaaabccddddde",
+		"abcd":       "abcd",
+		"45":         "Не верный формат",
+		`qwe\4\5`:    "qwe45",
+		`qwe\45`: 		"qwe44444",
+		`qwe5\4`:     "qweeeee4",
+		`qwe\\5`:     `qwe\\\\\`,
+		`q\we`:       "Не верный формат",
+		"ab/c":       "ab/c",
 	}
 
 	for key, val := range testStrings {
--- hw2/hw2.go
+++ hw2/hw2.go
@@ -8,7 +8,6 @@ import (
 )
 
 func repeat(v rune, r []rune) strings.Builder {
-
 	var result strings.Builder
 	cnt, _ := strconv.Atoi(string(r))
 	for i := 1; i < cnt; i++ {
@@ -18,7 +17,6 @@ func repeat(v rune, r []rune) strings.Builder {
 }
 
 func UnPackString(s string) (string, error) {
-
 	var result strings.Builder
 	v := []rune(s)
 	index := 0
@@ -26,12 +24,8 @@ func UnPackString(s string) (string, error) {
 	if unicode.IsDigit(v[0]) {
 		return "", fmt.Errorf("Не верный формат")
 	}
-
 	for i := 0; i < len(v); i++ {
 		r := v[i]
-		if i == 0 {
-			continue
-		}
 		if r == '\\' && !isSkype {
 			isSkype = true
 			if index > 0 {
@@ -41,35 +35,28 @@ func UnPackString(s string) (string, error) {
 			}
 			continue
 		}
-
 		if r == '\\' && isSkype {
 			isSkype = false
 		}
-
 		if !unicode.IsDigit(r) && r != '\\' && isSkype {
 			return "", fmt.Errorf("Не верный формат")
 		}
-
 		if unicode.IsDigit(r) && index == 0 && !isSkype {
 			index = i
 		}
-
 		if !unicode.IsDigit(r) && index > 0 {
 			//fmt.Printf("r=%c, index=%v\n", r, index)
 			s := repeat(v[index-1], v[index:i])
 			result.WriteString(s.String())
 			index = 0
 		}
-
 		if i == (len(v)-1) && index > 0 {
 			s := repeat(v[index-1], v[index:i+1])
 			result.WriteString(s.String())
 		}
-
 		if index > 0 {
 			continue
 		}
-
 		isSkype = false
 		result.WriteRune(r)
 	}
